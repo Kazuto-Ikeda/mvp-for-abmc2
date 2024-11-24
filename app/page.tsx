c9316d9f9e48918b1f58fbaaf5cae6f5e8188390
kazuto <taste.like.7@gmail.com>
2024-11-25 02:43:51 +0900

    change design top page

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,49 @@ export default function Home() {
   return (
     <div>
       <nav>
-        <h1>Welcome to the Auto create Business Model Campus</h1>
-        <ul>
-          <li><Link href="/home">Home</Link></li>
-          <li><Link href="/persona">Persona</Link></li>
-          <li><Link href="/persona-review">Persona Review</Link></li>
-          <li><Link href="/interview">Interview</Link></li>
-          <li><Link href="/review">Review</Link></li>
-          <li><Link href="/create-bmc">Create BMC</Link></li>
-        </ul>
+        <h1 style={{ textAlign: "center" }}>Welcome to the Auto create Business Model Campus</h1>
+        <div style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "16px",
+          marginTop: "20px",
+        }}>
+          <Link href="/home">
+            <a style={buttonStyle}>Home</a>
+          </Link>
+          <Link href="/persona">
+            <a style={buttonStyle}>Persona</a>
+          </Link>
+          <Link href="/persona-review">
+            <a style={buttonStyle}>Persona Review</a>
+          </Link>
+          <Link href="/interview">
+            <a style={buttonStyle}>Interview</a>
+          </Link>
+          <Link href="/review">
+            <a style={buttonStyle}>Review</a>
+          </Link>
+          <Link href="/create-bmc">
+            <a style={buttonStyle}>Create BMC</a>
+          </Link>
+        </div>
       </nav>
     </div>
   );
 }
+
+const buttonStyle = {
+  display: "inline-block",
+  padding: "10px 20px",
+  textAlign: "center",
+  textDecoration: "none",
+  color: "white",
+  backgroundColor: "#0070f3",
+  borderRadius: "5px",
+  transition: "background-color 0.3s",
+  cursor: "pointer",
+};
+
+buttonStyle[':hover'] = {
+  backgroundColor: "#005bb5",
+};
